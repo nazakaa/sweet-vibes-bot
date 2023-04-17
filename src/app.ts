359c0409247b579938f2f888f1514c0a246baa61
nazaka <nazar.kravchenko26@gmail.com>
2023-04-17 02:26:58 +0200

    added compliments api to the bot

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,27 @@
 import { Markup, Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
+import getCompliment from './service/complimentr-api';
 
-// init dotenv
+// initialize environment variables
 dotenv.config();
 
-// create bot
+// initialize the bot
 if (!process.env.BOT_TOKEN) throw new Error('BOT_TOKEN is not defined');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// define event handlers
+// HANDLERS
 bot.start((ctx) => {
-    ctx.reply('Hello ' + ctx.from.first_name + '!');
-});
-
-bot.help((ctx) => {
-    ctx.reply('Send /start to receive a greeting');
-    ctx.reply('Send /keyboard to receive a message with a keyboard');
-    ctx.reply('Send /quit to stop the bot');
-});
-
-bot.command('quit', (ctx) => {
-    // Explicit usage
-    ctx.telegram.leaveChat(ctx.message.chat.id);
-    // Context shortcut
-    ctx.leaveChat();
-});
-
-bot.command('keyboard', (ctx) => {
     ctx.reply(
-        'Keyboard',
-        Markup.inlineKeyboard([
-            Markup.button.callback('First option', 'first'),
-            Markup.button.callback('Second option', 'second'),
-        ])
+        `Hi ${ctx.from.first_name}! Would you like getting a compliment?`,
+        Markup.inlineKeyboard([{ callback_data: 'get_compliment', text: 'Yes pleeese!' }])
     );
 });
 
-bot.on('text', (ctx) => {
-    ctx.reply('You choose the ' + (ctx.message.text === 'first' ? 'First' : 'Second') + ' Option!');
+bot.on('callback_query', async (ctx) => {
+    if (ctx.callbackQuery) ctx.answerCbQuery();
+
+    const compliment = await getCompliment();
+    ctx.reply(compliment, Markup.inlineKeyboard([{ callback_data: 'get_compliment', text: 'Get another one' }]));
 });
 
 // launch the bot
