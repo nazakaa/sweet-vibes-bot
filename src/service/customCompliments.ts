4ad32921c102e9002a0e8bd2e15a1625b2eeeb19
nazaka <nazar.kravchenko26@gmail.com>
2023-04-20 20:02:42 +0200

    removed unused file

--- src/service/customCompliments.ts
+++
@@ -1,9 +0,0 @@
-import { getRandomElementOfArray } from '../utils/arrayRandomElement';
-
-const compliments = ['You look nice today', 'You are a smart cookie'];
-
-export const getCompliment = () => {
-    return getRandomElementOfArray(compliments);
-};
-
-export default getCompliment;
