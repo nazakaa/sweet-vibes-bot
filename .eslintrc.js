daf7c4c0947a57de4a431488cb1649315b179f9b
nazaka <nazar.kravchenko26@gmail.com>
2023-04-20 21:00:02 +0200

    new `eslint` rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
         '@typescript-eslint/no-floating-promises': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
     },
     parserOptions: {
         project: true,
